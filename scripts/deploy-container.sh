334a1cfb7090f136d869fd862d197550f09bed4b
Wisa Powthongchin <wisa.pow@gmail.com>
2018-10-17 17:41:08 +0700

    created automate deployment script

---
+++ scripts/deploy-container.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# disable user certificate
+gcloud config unset container/use_client_certificate
+# access cluster
+gcloud container clusters get-credentials salt-cluster --zone asia-southeast1-a --project isp-chat-app
+# pull image
+docker pull wisatong/saltwater:1.0-SNAPSHOT
+# set new image to deployment
+kubectl set image deployment/salt-web salt-web=wisatong/saltwater:1.0-SNAPSHOT
+# view successful deployment
+kubectl get services
