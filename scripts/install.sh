26d865f05af4a9aab280991f7c3e154f434bd77a
Wisa Powthongchin <wisa.pow@gmail.com>
2018-10-17 20:36:28 +0700

    removed kube config entriely

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,3 @@
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-
-mkdir ${HOME}/.kube
-cp config ${HOME}/.kube/config
