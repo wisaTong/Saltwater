075e64b3b7a7a137e6ef7b4ccb44f1e4d44fbf93
Wisa Powthongchin <wisa.pow@gmail.com>
2018-10-17 20:33:25 +0700

    removed curl .kube/config

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,8 +5,5 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s htt
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
-# download .kube/config
-curl -o config https://$GITHUB_ACCESS_TOKEN@raw.githubusercontent.com/wisaTong/Saltwater/feature-automate-deployment/.kube/config
-
 mkdir ${HOME}/.kube
 cp config ${HOME}/.kube/config
