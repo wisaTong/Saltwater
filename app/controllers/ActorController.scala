32c98e6c8d3510cb353d24a2cdb84556d57e9748
booknap <napasai45@gmail.com>
2018-10-19 19:55:18 +0700

    added Actor

---
+++ app/controllers/ActorController.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import play.api.mvc._
+import play.api.libs.streams.ActorFlow
+import javax.inject.Inject
+import akka.actor.ActorSystem
+import akka.stream.Materializer
+
+class ActorController @Inject()(cc:ControllerComponents) (implicit system: ActorSystem, mat: Materializer) extends AbstractController(cc) {
+  import actor.WebSocketActor
+  def socket = WebSocket.accept[String, String] { request =>
+    ActorFlow.actorRef { out =>
+      WebSocketActor.props(out)
+    }
+  }
+}
