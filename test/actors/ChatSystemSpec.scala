37bd33118e5dc9681826eda29e0560709615a93c
booknap <napasai45@gmail.com>
2018-11-04 19:34:07 +0700

    fixed race condition

--- test/actors/ChatSystemSpec.scala
+++ test/actors/ChatSystemSpec.scala
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit
 import actor.ChatSystem
 import akka.util.Timeout
 import base.UnitSpec
+import play.api.Logger
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.FiniteDuration
@@ -13,16 +14,15 @@ import scala.util.{Failure, Success}
 class ChatSystemSpec
   extends UnitSpec {
 
-  val TEST_ACTOR_NAME: String = "test"
-
   implicit val timeout: Timeout = Timeout(FiniteDuration(10, TimeUnit.SECONDS))
 
   "createRoom" should {
     "create actor in ChatSystem" in {
+      val TEST_ACTOR_NAME: String = "create"
 
       ChatSystem.createRoom(TEST_ACTOR_NAME)
 
-      ChatSystem.system.actorSelection("/user/test").resolveOne.onComplete {
+      ChatSystem.system.actorSelection(s"/user/$TEST_ACTOR_NAME").resolveOne.onComplete {
         case Success(actorRef) => actorRef.path.name mustBe TEST_ACTOR_NAME
         case _ => fail
       }
@@ -31,19 +31,18 @@ class ChatSystemSpec
 
   "deleteRoom" should {
     "delete actor in ChatSystem" in {
+      val TEST_ACTOR_NAME: String = "delete"
+
       ChatSystem.createRoom(TEST_ACTOR_NAME)
 
       ChatSystem.deleteRoom(TEST_ACTOR_NAME)
 
-      ChatSystem.system.actorSelection("/user/test").resolveOne.onComplete {
+      Thread.sleep(1000)
+      ChatSystem.system.actorSelection(s"/user/$TEST_ACTOR_NAME").resolveOne.onComplete {
         case Failure(_) => succeed
         case Success(_) => fail
       }
     }
-  }
-
-  override def afterEach(): Unit = {
-    ChatSystem.deleteRoom(TEST_ACTOR_NAME)
   }
 
 }
