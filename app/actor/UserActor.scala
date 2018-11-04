14ef4e156618982dad7e4163d2d0c26a4434ab7a
Sea <patompong.su@ku.th>
2018-11-04 13:00:38 +0700

    import json

--- app/actor/UserActor.scala
+++ app/actor/UserActor.scala
@@ -2,13 +2,14 @@ package actor
 
 import akka.actor._
 import models.Message
+import utillities.Json
 
 object UserActor {
   def props: Props = Props(new UserActor)
 }
 
 class UserActor extends Actor {
-  override def receiave: Receive = {
-      case msg: Message => ChatSystem.lounge ! toObject(msg)(Message).get
+  override def receive: Receive = {
+      case msg: String => ChatSystem.lounge ! Json.toObject[Message](msg)
   }
 }
