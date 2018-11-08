5bd621078a2cb5c4da27913dee96ee8873094634
First529 <first_dejabhol@hotmail.com>
2018-11-08 18:28:00 +0700

    throw new exception inside functions

--- app/actor/ChatSystem.scala
+++ app/actor/ChatSystem.scala
@@ -2,8 +2,9 @@ package actor
 
 import java.util.concurrent.TimeUnit
 
-import akka.actor.{ActorRef, ActorSystem, PoisonPill}
+import akka.actor.{ActorRef, ActorSystem, FSM, PoisonPill, Status}
 import akka.util.Timeout
+import definitions.ActorException.{ActorNameNotUniqueException, ActorNotFoundException}
 import play.api.Logger
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -15,15 +16,29 @@ object ChatSystem {
   implicit val timeout: Timeout = Timeout(FiniteDuration(10, TimeUnit.SECONDS))
 
   val system: ActorSystem = ActorSystem("ChatSystem")
-  val lounge: ActorRef = system.actorOf(ChatRoomActor.props)
+  val lounge: ActorRef = system.actorOf(ChatRoomActor.props, "lounge")
 
-  def createRoom(name: String): ActorRef =
-    system.actorOf(ChatRoomActor.props, name)
+  def createRoom(name: String): ActorRef = {
+    system.actorOf(ChatRoomActor.props, name) match {
+      case actor: ActorRef => {
+        Logger.info(s"successfully create actor ${actor.path.name}")
+        actor
+      }
+      case _ => throw new ActorNameNotUniqueException(name)
+    }
+  }
 
   def deleteRoom(name: String): Unit = {
     system.actorSelection(s"/user/$name").resolveOne.onComplete {
-      case Success(actor) => actor ! PoisonPill
-      case Failure(_) => Logger.warn(s"cannot find actor named '$name'")
+      case Success(actor) => {
+        actor ! PoisonPill
+        println("deleted")
+      }
+      case Failure(_) => {
+        throw new ActorNotFoundException()
+        Logger.warn(s"cannot find actor named '$name'")
+
+      }
     }
   }
 
