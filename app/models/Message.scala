5f007d6bc25d34c08d12c04ddbba3046747c5479
First529 <first_dejabhol@hotmail.com>
2018-11-09 16:31:50 +0700

    change string name

--- app/models/Message.scala
+++ app/models/Message.scala
@@ -3,7 +3,7 @@ package models
 import org.joda.time.DateTime
 import utillities.Json
 
-case class Message(send: String
+case class Message(sender: String
                    , message: String
                    , destination: String
                    , time: DateTime = DateTime.now()
