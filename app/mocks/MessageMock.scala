c1c6e6e5a53d4d7d5f22be7ba2fdea647f6d82a8
First529 <first_dejabhol@hotmail.com>
2018-11-11 12:42:48 +0700

    minor changes

--- app/mocks/MessageMock.scala
+++ app/mocks/MessageMock.scala
@@ -8,6 +8,7 @@ object MessageMock {
   val messageForm: Message =
     Message("6010545811"
       , "Test Message"
+      , "lounge"
       , DateTime.now
     )
 
@@ -16,14 +17,16 @@ object MessageMock {
       , "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, " +
         "ratione fugit. Atque commodi autem velit sit est dolores aut dignissimos, " +
         "tenetur, impedit quaerat quo odio aliquam optio reiciendis, in id?"
+      , "lounge"
       , DateTime.now
     )
 
   val messageJsonForm: String =
     """
       | {
-      |   "id": "6010545811"
+      |   "sender": "6010545811"
       |   , "message": "Test Message"
+      |   , "destination": "lounge"
       |   , "time": "1999-05-29T07:42:22Z"
       | }
     """.stripMargin
