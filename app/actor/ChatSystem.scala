f7454dcfc254fd53f8f4dba55fea4b381985588c
Wisa Powthongchin <wisa.pow@gmail.com>
2018-11-13 09:34:11 +0700

    removed unnecessary logging

--- app/actor/ChatSystem.scala
+++ app/actor/ChatSystem.scala
@@ -2,9 +2,9 @@ package actor
 
 import java.util.concurrent.TimeUnit
 
-import akka.actor.{ActorRef, ActorSystem, FSM, PoisonPill, Status}
+import akka.actor.{ActorRef, ActorSystem, PoisonPill}
 import akka.util.Timeout
-import definitions.ActorException.{ActorNameNotUniqueException, ActorNotFoundException}
+import definitions.ActorException.ActorNotFoundException
 import play.api.Logger
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -19,25 +19,15 @@ object ChatSystem {
   val lounge: ActorRef = system.actorOf(ChatRoomActor.props, "lounge")
 
   def createRoom(name: String): ActorRef = {
-    system.actorOf(ChatRoomActor.props, name) match {
-      case actor: ActorRef => {
-        Logger.info(s"successfully create actor ${actor.path.name}")
-        actor
-      }
-      case _ => throw new ActorNameNotUniqueException(name)
-    }
+    system.actorOf(ChatRoomActor.props, name)
   }
 
   def deleteRoom(name: String): Unit = {
     system.actorSelection(s"/user/$name").resolveOne.onComplete {
-      case Success(actor) => {
-        actor ! PoisonPill
-        println("deleted")
-      }
+      case Success(actor) => actor ! PoisonPill
       case Failure(_) => {
-        throw new ActorNotFoundException()
         Logger.warn(s"cannot find actor named '$name'")
-
+        throw new ActorNotFoundException()
       }
     }
   }
