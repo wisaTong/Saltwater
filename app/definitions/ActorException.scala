54b4a9e4ebedf3dd8b9a8a1dd9f8dde97c577570
Wisa Powthongchin <wisa.pow@gmail.com>
2018-11-23 10:02:16 +0700

    Improve exception message

--- app/definitions/ActorException.scala
+++ app/definitions/ActorException.scala
@@ -2,8 +2,8 @@ package definitions
 
 object ActorException {
 
-  class ActorNotFoundException()
-    extends Exception("This actor doesn't exist.")
+  class ActorNotFoundException(s: String)
+    extends Exception(s"actor with name [$s] doesn't exist.")
 
   class ActorNameNotUniqueException(s: String)
     extends Exception(s"This name ($s) has already been used.")
