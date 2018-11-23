041c85017796e827c7ed643fbaedfe54d359e9d7
Wisa Powthongchin <wisa.pow@gmail.com>
2018-11-23 10:03:02 +0700

    catching error

--- app/actor/ChatSystem.scala
+++ app/actor/ChatSystem.scala
@@ -4,12 +4,11 @@ import java.util.concurrent.TimeUnit
 
 import akka.actor.{ActorRef, ActorSystem, PoisonPill}
 import akka.util.Timeout
-import definitions.ActorException.ActorNotFoundException
-import play.api.Logger
+import definitions.ActorException.{ActorNameNotUniqueException, ActorNotFoundException}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.FiniteDuration
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 
 object ChatSystem {
 
@@ -19,16 +18,18 @@ object ChatSystem {
   val lounge: ActorRef = system.actorOf(ChatRoomActor.props, "lounge")
 
   def createRoom(name: String): ActorRef = {
-    system.actorOf(ChatRoomActor.props, name)
+    Try {
+      system.actorOf(ChatRoomActor.props, name)
+    } match {
+      case Success(ref) => ref
+      case Failure(_) => throw new ActorNameNotUniqueException(name)
+    }
   }
 
   def deleteRoom(name: String): Unit = {
     system.actorSelection(s"/user/$name").resolveOne.onComplete {
       case Success(actor) => actor ! PoisonPill
-      case Failure(_) => {
-        Logger.warn(s"cannot find actor named '$name'")
-        throw new ActorNotFoundException()
-      }
+      case Failure(_) => throw new ActorNotFoundException(name)
     }
   }
 
