7dbf76c1d5e7f4b33adb827246435c6b588c7b9b
Wisa Powthongchin <wisa.pow@gmail.com>
2018-11-23 10:02:39 +0700

    Improve test

--- test/definitions/ActorExceptionSpec.scala
+++ test/definitions/ActorExceptionSpec.scala
@@ -3,46 +3,53 @@ package definitions
 import java.util.concurrent.TimeUnit
 
 import actor.ChatSystem
-import play.api.Logger
 import akka.util.Timeout
 import base.UnitSpec
 import definitions.ActorException.{ActorNameNotUniqueException, ActorNotFoundException}
 
+import scala.concurrent.Promise
 import scala.concurrent.duration.FiniteDuration
+import scala.util.{Failure, Success, Try}
+import scala.concurrent.ExecutionContext.Implicits.global
 
-class ActorExceptionSpec extends UnitSpec{
+class ActorExceptionSpec extends UnitSpec {
 
   implicit val timeout: Timeout = Timeout(FiniteDuration(10, TimeUnit.SECONDS))
 
   val TEST_ACTOR_NAME: String = "test"
 
-  "duplicateRoom" should {
+  "create room with same name" should {
     "throw ActorNameNotUniqueException" in {
 
-      ChatSystem.createRoom(TEST_ACTOR_NAME)
-
-      try {
+      Try {
         ChatSystem.createRoom(TEST_ACTOR_NAME)
-      } catch {
-        case _: Exception => Logger.info(new ActorNameNotUniqueException("test").getMessage)
+        ChatSystem.createRoom(TEST_ACTOR_NAME)
+      } match {
+        case Failure(ex) => ex.getClass mustBe classOf[ActorNameNotUniqueException]
+        case Success(_) => fail
       }
 
       ChatSystem.deleteRoom(TEST_ACTOR_NAME)
 
-
     }
   }
 
-  "roomNotFound" should {
-    "throw ActorNameFoundException" in {
+  // TODO improve this test (this test is incomplete)
+  "search for room that does not exist" should {
+    "throw ActorNotFoundException" in {
 
-      try {
-        ChatSystem.deleteRoom(TEST_ACTOR_NAME)
-      } catch {
-        case _: Exception => Logger.info(new ActorNotFoundException().getMessage)
+      val deleteRoom = {
+        val promise = Promise[Unit]
+        promise success ChatSystem.deleteRoom(TEST_ACTOR_NAME)
+        promise
       }
 
+      deleteRoom.future.onComplete {
+        case Failure(ex) => ex.getClass mustBe classOf[ActorNotFoundException]
+        case Success(_) => fail
+      }
 
+      Thread.sleep(500)
     }
   }
 
