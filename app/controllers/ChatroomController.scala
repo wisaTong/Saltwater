97a3b42df5f2e370947c5f140a7ad9e23c8adf85
booknap <napasai45@gmail.com>
2018-12-03 16:12:39 +0700

    added getUserCount api

--- app/controllers/ChatroomController.scala
+++ app/controllers/ChatroomController.scala
@@ -1,9 +1,19 @@
 package controllers
 
+import java.util.concurrent.TimeUnit
+
 import actor.ChatSystem
+import akka.actor.ActorRef
 import javax.inject.{Inject, Singleton}
-import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.mvc.{AbstractController, ControllerComponents, Result}
 import services.FirebaseService
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import akka.pattern.ask
+import akka.util.Timeout
+
+import scala.reflect.ClassTag
 
 @Singleton
 class ChatroomController @Inject()(cc: ControllerComponents)
@@ -26,4 +36,15 @@ class ChatroomController @Inject()(cc: ControllerComponents)
 
     Ok(s"$name chat room deleted")
   }
+
+  implicit val timeout: Timeout = Timeout(FiniteDuration(10, TimeUnit.SECONDS))
+
+  def getUserCount(roomName: String) = Action { request =>
+    val ref: ActorRef = Await.result(ChatSystem.getActor(roomName), 1 second)
+    val userCount: Int = Await.result(ref ? UserCount() mapTo ClassTag(classOf[Int]), 1 second)
+    Ok(userCount.toString)
+  }
+
 }
+
+case class UserCount()
