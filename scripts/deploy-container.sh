dda1b7971bfe369d557883e88ac368b217cecc75
Wisa Powthongchin <wisa.pow@gmail.com>
2018-12-04 15:32:45 +0700

    updated image tag

--- scripts/deploy-container.sh
+++ scripts/deploy-container.sh
@@ -5,8 +5,8 @@ gcloud config unset container/use_client_certificate
 # access cluster
 gcloud container clusters get-credentials salt-cluster --zone asia-southeast1-a --project isp-chat-app
 # pull image
-docker pull wisatong/saltwater:0.6.4-SNAPSHOT
+docker pull wisatong/saltwater:0.7.1-SNAPSHOT
 # set new image to deployment
-kubectl set image deployment/salt-web salt-web=wisatong/saltwater:0.6.4-SNAPSHOT
+kubectl set image deployment/salt-web salt-web=wisatong/saltwater:0.7.1-SNAPSHOT
 # view successful deployment
 kubectl get services
